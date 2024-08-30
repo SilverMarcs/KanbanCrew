48d5b2ca78324ba6d5ed02dd56133cca2e90d096
sfer0059 <sfer0059@student.monash.edu>
2024-08-31 02:44:33 +0800

    Add task expanded card

--- app/src/components/TaskCard.tsx
+++ app/src/components/TaskCard.tsx
@@ -9,6 +9,8 @@ import {
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { EllipsisIcon } from "lucide-react";
+import { TaskCardExpanded } from "./TaskCardExpanded";
+import { getTagColor, getPriorityColor } from "@/lib/utils";
 
 export interface TaskCardProps {
   index: number;
@@ -26,42 +28,6 @@ export interface TaskCardProps {
     | "Framework";
 }
 
-// Function to map tags to their respective colors
-const getTagColor = (tag: TaskCardProps["tag"]) => {
-  switch (tag) {
-    case "Frontend":
-      return { bgColor: "bg-blue-300", textColor: "text-blue-600" };
-    case "Backend":
-      return { bgColor: "bg-green-300", textColor: "text-green-600" };
-    case "API":
-      return { bgColor: "bg-yellow-300", textColor: "text-yellow-600" };
-    case "Database":
-      return { bgColor: "bg-red-300", textColor: "text-red-600" };
-    case "Testing":
-      return { bgColor: "bg-purple-300", textColor: "text-purple-600" };
-    case "UI/UX":
-      return { bgColor: "bg-pink-300", textColor: "text-pink-600" };
-    case "Framework":
-      return { bgColor: "bg-indigo-300", textColor: "text-indigo-600" };
-    default:
-      return { bgColor: "bg-gray-300", textColor: "text-gray-600" };
-  }
-};
-
-// Function to map priorities to their respective colors
-const getPriorityColor = (priority: TaskCardProps["priority"]) => {
-  switch (priority) {
-    case "Important":
-      return { bgColor: "bg-orange-400", textColor: "text-orange-800" };
-    case "Urgent":
-      return { bgColor: "bg-red-400", textColor: "text-red-800" };
-    case "Low":
-      return { bgColor: "bg-green-300", textColor: "text-green-600" };
-    default:
-      return { bgColor: "bg-gray-300", textColor: "text-gray-600" };
-  }
-};
-
 export const TaskCard = ({
   index,
   title,
@@ -70,7 +36,7 @@ export const TaskCard = ({
   avatarUrl,
   tag,
 }: TaskCardProps) => {
-  const { bgColor, textColor } = getTagColor(tag);
+  const { bgColor: tagBgColor, textColor: tagTextColor } = getTagColor(tag);
   const { bgColor: priorityBgColor, textColor: priorityTextColor } =
     getPriorityColor(priority);
 
@@ -97,7 +63,7 @@ export const TaskCard = ({
           </CardContent>
           <CardFooter className="flex w-full justify-end">
             <div
-              className={`${bgColor} ${textColor} py-2 px-4 rounded-3xl text-xs font-bold absolute bottom-3 right-3`}
+              className={`${tagBgColor} ${tagTextColor} py-2 px-4 rounded-3xl text-xs font-bold absolute bottom-3 right-3`}
             >
               {tag}
             </div>
@@ -110,7 +76,9 @@ export const TaskCard = ({
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
         <DialogDescription>
-          This is the detailed description of card {title}.
+          <TaskCardExpanded
+            {...{ index, title, storyPoints, priority, avatarUrl, tag }}
+          />
         </DialogDescription>
       </DialogContent>
     </Dialog>
--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,6 +1,43 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { TaskCardProps } from "../components/TaskCard";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Function to map tags to their respective colors
+export const getTagColor = (tag: TaskCardProps["tag"]) => {
+  switch (tag) {
+    case "Frontend":
+      return { bgColor: "bg-blue-300", textColor: "text-blue-600" };
+    case "Backend":
+      return { bgColor: "bg-green-300", textColor: "text-green-600" };
+    case "API":
+      return { bgColor: "bg-yellow-300", textColor: "text-yellow-600" };
+    case "Database":
+      return { bgColor: "bg-red-300", textColor: "text-red-600" };
+    case "Testing":
+      return { bgColor: "bg-purple-300", textColor: "text-purple-600" };
+    case "UI/UX":
+      return { bgColor: "bg-pink-300", textColor: "text-pink-600" };
+    case "Framework":
+      return { bgColor: "bg-indigo-300", textColor: "text-indigo-600" };
+    default:
+      return { bgColor: "bg-gray-300", textColor: "text-gray-600" };
+  }
+};
+
+// Function to map priorities to their respective colors
+export const getPriorityColor = (priority: TaskCardProps["priority"]) => {
+  switch (priority) {
+    case "Important":
+      return { bgColor: "bg-orange-400", textColor: "text-orange-800" };
+    case "Urgent":
+      return { bgColor: "bg-red-400", textColor: "text-red-800" };
+    case "Low":
+      return { bgColor: "bg-green-300", textColor: "text-green-600" };
+    default:
+      return { bgColor: "bg-gray-300", textColor: "text-gray-600" };
+  }
+};
