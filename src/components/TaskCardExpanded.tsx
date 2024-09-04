ad6e6680e44dacc58a5cdcef58feb47c3f4e5687
sfer0059 <sfer0059@student.monash.edu>
2024-09-05 01:23:15 +0800

    Add history log after fixing merge conflicts

--- src/components/TaskCardExpanded.tsx
+++ src/components/TaskCardExpanded.tsx
@@ -25,48 +25,57 @@ export const TaskCardExpanded = ({
   const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
 
   return (
-    <div className="text-start px-3">
-      <div className="w-full flex items-center space-x-2">
-        <PriorityDropdown
-          priority={priority}
-          setPriority={setPriority}
-          taskId={id}
-        />
-      </div>
-      <div className="flex space-x-2 align-middle items-center mt-2">
-        <p className="text-3xl text-black font-bold ">{title}</p>
-        <p> - {type}</p>
-      </div>
-      <div className="flex space-x-2">
-        <p className="text-gray-500 font-bold">{storyPoints} SP</p>
-        <p className="font-bold">- {status}</p>
-      </div>
-      <p className="text-muted-foreground font-semibold mt-6">Assignee</p>
-      <div className="mt-2 flex space-x-2 w-full items-center">
-        <Avatar>
-          <AvatarImage src={avatarUrl} />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        <p className="font-semibold">{assignee}</p>
-      </div>
-      <div className="mt-6">
-        <p className="font-bold text-xl">Description</p>
-        <p className="text-gray-600">{description}</p>
-      </div>
-      <div className="mt-20 flex justify-between items-center">
-        <div className="flex space-x-3 items-center">
-          <p className="font-semobold text-gray-600">Project stage</p>
-          <p className="font-bold">{projectStage}</p>
+    <div className="px-3">
+      <div className="flex">
+        <div className="text-start">
+          <div className="w-full flex items-center space-x-2">
+            <PriorityDropdown
+              priority={priority}
+              setPriority={setPriority}
+              taskId={id}
+            />
+          </div>
+          <div className="flex space-x-2 align-middle items-center mt-2">
+            <p className="text-3xl text-black font-bold ">{title}</p>
+            <p> - {type}</p>
+          </div>
+          <div className="flex space-x-2">
+            <p className="text-gray-500 font-bold">{storyPoints} SP</p>
+            <p className="font-bold">- {status}</p>
+          </div>
+          <p className="text-muted-foreground font-semibold mt-6">Assignee</p>
+          <div className="mt-2 flex space-x-2 w-full items-center">
+            <Avatar>
+              <AvatarImage src={avatarUrl} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p className="font-semibold">{assignee}</p>
+          </div>
+          <div className="mt-6">
+            <p className="font-bold text-xl">Description</p>
+            <p className="text-gray-600">{description}</p>
+          </div>
         </div>
-        <div className="flex items-center space-x-2">
-          {selectedTags.map((tag, i) => (
-            <TagBadge key={i} tag={tag} />
-          ))}
-          <TagDropdown
-            selectedTags={selectedTags}
-            onTagChange={setSelectedTags}
-            taskId={id}
-          />
+        <div className="mt-14 ml-10">
+          <HistoryLogs />
+        </div>
+      </div>
+      <div>
+        <div className="mt-20 flex justify-between items-center">
+          <div className="flex space-x-3 items-center">
+            <p className="font-semobold text-gray-600">Project stage</p>
+            <p className="font-bold">{projectStage}</p>
+          </div>
+          <div className="flex items-center space-x-2">
+            {selectedTags.map((tag, i) => (
+              <TagBadge key={i} tag={tag} />
+            ))}
+            <TagDropdown
+              selectedTags={selectedTags}
+              onTagChange={setSelectedTags}
+              taskId={id}
+            />
+          </div>
         </div>
       </div>
     </div>
