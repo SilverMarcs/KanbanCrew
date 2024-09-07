e4190b36494767473738e5a37cb3370b5597bce6
sfer0059 <sfer0059@student.monash.edu>
2024-09-07 19:50:03 +0800

    Move functions to utils.ts

--- src/components/HistoryLogs.tsx
+++ src/components/HistoryLogs.tsx
@@ -4,6 +4,7 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { HistoryLog } from "@/models/HistoryLog";
 import { Member } from "@/models/Member";
+import { convertToDate, getRelativeTime } from "@/lib/utils";
 
 const HistoryLogs: React.FC<
   { historyLogs: HistoryLog[] } & { members: Member[] }
@@ -17,13 +18,6 @@ const HistoryLogs: React.FC<
       : { firstName: "Unknown", lastName: "Member" };
   };
 
-  const convertToDate = (time: {
-    seconds: number;
-    nanoseconds: number;
-  }): Date => {
-    return new Date(time.seconds * 1000); // Convert seconds to milliseconds
-  };
-
   useEffect(() => {
     if (!historyLogs) {
       return;
@@ -33,31 +27,6 @@ const HistoryLogs: React.FC<
     });
     setSortedLogs(sorted);
   }, [historyLogs]);
-
-  // Function to calculate relative time
-  const getRelativeTime = (timestamp: Date): string => {
-    const now = new Date();
-    const diff = Math.abs(now.getTime() - timestamp.getTime());
-    const minutes = Math.floor(diff / 60000);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const months = Math.floor(days / 30);
-    const years = Math.floor(months / 12);
-
-    if (years > 0) {
-      return `${years} year${years > 1 ? "s" : ""} ago`;
-    } else if (months > 0) {
-      return `${months} month${months > 1 ? "s" : ""} ago`;
-    } else if (days > 0) {
-      return `${days} day${days > 1 ? "s" : ""} ago`;
-    } else if (hours > 0) {
-      return `${hours} hour${hours > 1 ? "s" : ""} ago`;
-    } else if (minutes > 0) {
-      return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
-    } else {
-      return "Just now";
-    }
-  };
 
   return (
     <div>
