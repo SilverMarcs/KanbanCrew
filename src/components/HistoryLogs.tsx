78eb3be18ad53f17317f75acea628f8bf0e304b9
sfer0059 <sfer0059@student.monash.edu>
2024-09-07 19:00:25 +0800

    Handle case when no logs are passed in

--- src/components/HistoryLogs.tsx
+++ src/components/HistoryLogs.tsx
@@ -25,6 +25,9 @@ const HistoryLogs: React.FC<
   };
 
   useEffect(() => {
+    if (!historyLogs) {
+      return;
+    }
     const sorted = [...historyLogs].sort((a, b) => {
       return convertToDate(b.time).getTime() - convertToDate(a.time).getTime();
     });
@@ -62,28 +65,35 @@ const HistoryLogs: React.FC<
         <History size={28} />
         <div className="font-semibold text-lg">History log</div>
       </div>
+
       <ScrollArea className="mt-2 ml-16 flex flex-col max-h-60 min-w-full">
-        {sortedLogs.map((log: HistoryLog, index) => (
-          <div key={index} className="flex space-x-2">
-            <div className="h-12 w-0.5 mr-0.5 bg-gray-400 opacity-80"></div>
-            <Avatar>
-              <AvatarImage src={""} />
-              <AvatarFallback>
-                {getMemberName(log.member.id).firstName[0]}
-                {getMemberName(log.member.id).lastName[0]}
-              </AvatarFallback>
-            </Avatar>
-            <div className="flex flex-col min-w-28">
-              <div className="text-xs">
-                {getRelativeTime(convertToDate(log.time))}
-              </div>
-              <div className="text-black font-bold">
-                {getMemberName(log.member.id).firstName}{" "}
-                {getMemberName(log.member.id).lastName}
+        {sortedLogs.length > 0 ? (
+          sortedLogs.map((log: HistoryLog, index) => (
+            <div key={index} className="flex space-x-2">
+              <div className="h-12 w-0.5 mr-0.5 bg-gray-400 opacity-80"></div>
+              <Avatar>
+                <AvatarImage src={""} />
+                <AvatarFallback>
+                  {getMemberName(log.member.id).firstName[0]}
+                  {getMemberName(log.member.id).lastName[0]}
+                </AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col min-w-28">
+                <div className="text-xs">
+                  {getRelativeTime(convertToDate(log.time))}
+                </div>
+                <div className="text-black font-bold">
+                  {getMemberName(log.member.id).firstName}{" "}
+                  {getMemberName(log.member.id).lastName}
+                </div>
               </div>
             </div>
+          ))
+        ) : (
+          <div className="text-center text-gray-500 mt-4">
+            No logs available
           </div>
-        ))}
+        )}
       </ScrollArea>
     </div>
   );
