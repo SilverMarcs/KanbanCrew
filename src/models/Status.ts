6eacb5c14ddd09ebfdcdb606d7a01f2736239908
Zabir Raihan <zrai0002@student.monash.edu>
2024-09-16 02:39:50 +0800

    Reusable component for viewing Status

--- src/models/Status.ts
+++ src/models/Status.ts
@@ -1,5 +1,16 @@
 export enum Status {
-    NotStarted = "Not Started",
-    InProgress = "In Progress",
-    Completed = "Completed",
+  NotStarted = "Not Started",
+  InProgress = "In Progress",
+  Completed = "Completed",
 }
+
+export const getStatusColor = (status: Status) => {
+  switch (status) {
+    case Status.NotStarted:
+      return "#FF6E6E";
+    case Status.InProgress:
+      return "#FFA500";
+    case Status.Completed:
+      return "#34CB5E";
+  }
+};
