3e018813b8497c61efffd71cc5a4ed83ae8335f1
sfer0059 <sfer0059@student.monash.edu>
2024-09-17 00:56:20 +0800

    Match sprints in SprintList to design in Figma

--- src/app/sprints/SprintList.tsx
+++ src/app/sprints/SprintList.tsx
@@ -1,31 +1,28 @@
 import React from "react";
 import { useSprints } from "@/hooks/useSprints";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { StatusBadge } from "@/components/StatusBadge";
 
 const SprintList: React.FC = () => {
   const sprints = useSprints();
 
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="flex flex-col space-y-4 my-4">
       {sprints.map((sprint) => (
-        <Card key={sprint.id}>
-          <CardHeader>
-            <CardTitle className="text-xl">{sprint.name}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="mb-3">
+        <Card
+          key={sprint.id}
+          className="flex items-center w-full bg-yellow-200 outline-none border-0"
+        >
+          <div className="px-16 py-4 flex space-x-16 items-center">
+            <div className="text-xl font-bold">{sprint.name}</div>
+            <div className="font-bold">
               <StatusBadge status={sprint.status} />
             </div>
-            <p className="text-sm text-gray-600 mb-1">
-              <span className="font-semibold">Start Date:</span>{" "}
-              {sprint.startDate.toDate().toLocaleDateString()}
-            </p>
             <p className="text-sm text-gray-600">
-              <span className="font-semibold">End Date:</span>{" "}
+              {sprint.startDate.toDate().toLocaleDateString()} -{" "}
               {sprint.endDate.toDate().toLocaleDateString()}
             </p>
-          </CardContent>
+          </div>
         </Card>
       ))}
     </div>
