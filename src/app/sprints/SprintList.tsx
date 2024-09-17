8366b15ba22adb489d43745cec7359b3503654ca
sfer0059 <sfer0059@student.monash.edu>
2024-09-17 14:13:58 +0800

    Sort sprints based off starting date

--- src/app/sprints/SprintList.tsx
+++ src/app/sprints/SprintList.tsx
@@ -5,9 +5,14 @@ import SprintCard from "./SprintCard";
 const SprintList: React.FC = () => {
   const sprints = useSprints();
 
+  // Sort sprints by startDate
+  const sortedSprints = sprints.sort(
+    (a, b) => a.startDate.toMillis() - b.startDate.toMillis()
+  );
+
   return (
     <div className="flex flex-col space-y-4 my-4">
-      {sprints.map((sprint) => (
+      {sortedSprints.map((sprint) => (
         <SprintCard key={sprint.id} sprint={sprint} />
       ))}
       <CreateSprintCard />
