256642ffb1d79040d8671c587dcaa99526edb64d
hyba99 <hibazaman2@gmail.com>
2024-09-21 22:25:06 +0500

    Adding new isSprint prop to verify sprints

--- src/models/sprints/SprintFormProps.ts
+++ src/models/sprints/SprintFormProps.ts
@@ -1,10 +1,11 @@
-import { Status } from "../Status";
+import { TaskOrSprintStatus } from "@/components/TaskStatusDropdown";
 
 export interface SprintFormProps {
   initialTitle: string;
-  initialStatus: Status;
+  initialStatus: TaskOrSprintStatus; // Updated type
   initialStartDate?: Date;
   initialEndDate?: Date;
-  onSubmit: (title: string, status: Status, from: Date, to: Date) => void;
+  onSubmit: (title: string, status: TaskOrSprintStatus, from: Date, to: Date) => void; // Updated type
   submitButtonLabel: string;
+  isSprint: boolean;
 }
