4ea3d172980de3d1a6e5b2c6f3e167b20c15b825
hyba99 <hibazaman2@gmail.com>
2024-09-21 22:34:47 +0500

    Updating StatusBadge to accept both Status and SprintStatus types for tasks and sprints

--- src/components/StatusBadge.tsx
+++ src/components/StatusBadge.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { Status, getStatusColor } from "@/models/Status";
+import { SprintStatus, getSprintStatusColor } from "@/models/sprints/SprintStatus";
+
+type StatusOrSprintStatus = Status | SprintStatus;
 
 interface StatusBadgeProps {
-  status: Status;
+  status: StatusOrSprintStatus;
 }
 
 export const StatusBadge: React.FC<StatusBadgeProps> = ({ status }) => {
+  const getColor = (status: StatusOrSprintStatus) => {
+    if (Object.values(Status).includes(status as Status)) {
+      return getStatusColor(status as Status);
+    } else if (Object.values(SprintStatus).includes(status as SprintStatus)) {
+      return getSprintStatusColor(status as SprintStatus);
+    }
+    return "#000"; 
+  };
+
   return (
     <div className="flex items-center">
       <div
         className="w-2 h-2 rounded-full inline-block mr-2"
-        style={{ backgroundColor: getStatusColor(status) }}
+        style={{ backgroundColor: getColor(status) }}
       />
       <span>{status}</span>
     </div>
