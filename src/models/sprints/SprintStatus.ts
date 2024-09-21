f816dae0132165d882f6da1d5253c205dccfa74f
hyba99 <hibazaman2@gmail.com>
2024-09-21 22:18:12 +0500

    Added new SprintStatus enum for sprints

--- src/models/sprints/SprintStatus.ts
+++ src/models/sprints/SprintStatus.ts
@@ -1,5 +1,16 @@
-// export enum SprintStatus {
-//   NotStarted = "Not Started",
-//   Active = "Active",
-//   Completed = "Completed",
-// }
+export enum SprintStatus {
+  NotStarted = "Not Started",
+  Active = "Active",
+  Done = "Done",
+}
+
+export const getSprintStatusColor = (status: SprintStatus) => {
+  switch (status) {
+    case SprintStatus.NotStarted:
+      return "#FF6E6E";
+    case SprintStatus.Active:
+      return "#FFA500";
+    case SprintStatus.Done:
+      return "#34CB5E";
+  }
+};
