fb1cf13e43f84bceecdda65e60f4d81eb3446b4f
sfer0059 <sfer0059@student.monash.edu>
2024-09-21 19:07:37 +0800

    Create compact task card component

---
+++ src/components/TaskCardCompact.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Card, CardContent } from "@/components/ui/card";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Scroll, Bug } from "lucide-react";
+import { TaskCardExpanded } from "./TaskCardExpanded";
+import { Task } from "@/models/Task";
+import { Member } from "@/models/Member";
+import { getPriorityColor } from "@/models/Priority";
+import { TagDot } from "./TagDot";
+import { truncateText } from "@/lib/utils";
+import { Type } from "@/models/Type";
+
+interface TaskCardProps {
+  task: Task; // Changed to pass full Task object
+  members: Member[];
+  topTrailingChild?: React.ReactNode;
+}
+
+export const TaskCardCompact: React.FC<TaskCardProps> = ({
+  task,
+  members,
+  topTrailingChild,
+}) => {
+  const {
+    id,
+    index,
+    title,
+    storyPoints,
+    priority,
+    avatarUrl,
+    tags,
+    assignee,
+    description,
+    projectStage,
+    status,
+    type,
+    creationDate,
+    historyLogs,
+  } = task;
+
+  const { bgColor: priorityBgColor, textColor: priorityTextColor } =
+    getPriorityColor(priority);
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+  };
+
+  const TypeIcon = type === Type.UserStory ? Scroll : Bug;
+  const typeIconColor =
+    type === Type.UserStory ? "text-blue-500" : "text-orange-500";
+
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger className="w-96">
+        <Card className="relative rounded-xl">
+          <CardContent className="text-start py-3 px-4">
+            <div className="w-full flex justify-between items-center">
+              <div className="flex items-center gap-3">
+                <p
+                  className={`${priorityBgColor} ${priorityTextColor} py-1 px-3 rounded-md w-fit text-sm font-bold`}
+                >
+                  {priority}
+                </p>
+                <TypeIcon className={`${typeIconColor}`} size={16} />
+                <Avatar>
+                  <AvatarImage src={avatarUrl} />
+                  <AvatarFallback>
+                    {assignee.firstName[0]}
+                    {assignee.lastName[0]}
+                  </AvatarFallback>
+                </Avatar>
+              </div>
+              {topTrailingChild}
+            </div>
+            <div className="text-xl font-bold mt-2">{truncateText(title)}</div>
+            <div className="flex justify-between">
+              <div className="text-gray-500">Story point - {storyPoints}</div>
+              <div className="flex space-x-1">
+                {tags.map((tag) => (
+                  <TagDot key={tag} tag={tag} />
+                ))}
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </DialogTrigger>
+      <DialogContent className="bg-white max-w-3xl">
+        <DialogTitle className="hidden" />
+        <DialogDescription>
+          <TaskCardExpanded
+            id={id}
+            index={index}
+            title={title}
+            storyPoints={storyPoints}
+            priority={priority}
+            avatarUrl={avatarUrl}
+            tags={tags}
+            assignee={assignee}
+            description={description}
+            projectStage={projectStage}
+            status={status}
+            type={type}
+            creationDate={creationDate}
+            closeDialog={closeDialog}
+            historyLogs={historyLogs}
+            members={members}
+          />
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+};
