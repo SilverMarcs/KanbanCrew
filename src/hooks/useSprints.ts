eeecf177d720b7246868f0bd4f0830c57317cfe2
hyba99 <hibazaman2@gmail.com>
2024-09-25 16:33:59 +0500

    Update useSprints to change sprint status automatically according to the currentDate

--- src/hooks/useSprints.ts
+++ src/hooks/useSprints.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { collection, onSnapshot, Timestamp } from "firebase/firestore";
+import { collection, onSnapshot, Timestamp, updateDoc, doc } from "firebase/firestore";
 import { db } from "@/lib/firebaseConfig";
 import { Sprint } from "@/models/sprints/Sprint";
 import { SprintStatus } from "@/models/sprints/SprintStatus";
@@ -10,18 +10,38 @@ export const useSprints = () => {
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, "sprints"), (snapshot) => {
       const sprintsData: Sprint[] = [];
+      const currentDate = new Date();
 
       for (const docSnapshot of snapshot.docs) {
         const data = docSnapshot.data();
+        const startDate = data.startDate.toDate();
+        const endDate = data.endDate.toDate();
+        let status = data.sprintStatus as SprintStatus;
 
+        // Determine if the sprint is active or done
+        if (currentDate >= startDate && currentDate <= endDate) {
+          status = SprintStatus.Active;
+        } else if (currentDate > endDate) {
+          status = SprintStatus.Done;
+        }
+
+        
         sprintsData.push({
           id: docSnapshot.id,
           name: data.name,
-          status: data.sprintStatus as SprintStatus,
+          status: status,
           startDate: data.startDate as Timestamp,
           endDate: data.endDate as Timestamp,
           taskIds: data.taskIds || [],
         });
+
+        // Update Firestore 
+        if (status !== data.sprintStatus) {
+          const sprintRef = doc(db, "sprints", docSnapshot.id);
+          updateDoc(sprintRef, { sprintStatus: status }).catch((error) => {
+            console.error("Error updating sprint status: ", error);
+          });
+        }
       }
 
       setSprints(sprintsData);
@@ -31,4 +51,4 @@ export const useSprints = () => {
   }, []);
 
   return sprints;
-};
+}; 
